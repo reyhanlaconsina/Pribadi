21fed812f55b5132843edd5dc17b3c0a76763430
reyhanlatuconsina <55250316+reyhanlaconsina@users.noreply.github.com>
2022-04-19 22:01:46 +0700

    halaman 2

--- shaumgram_project/2.dart
+++ shaumgram_project/2.dart
@@ -7,7 +7,6 @@ class HalamanDua extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
         body: SafeArea(
       child: Column(
         children: [
